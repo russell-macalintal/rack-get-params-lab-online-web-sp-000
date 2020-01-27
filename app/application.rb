bfbdbb89d5e2757f9161356a8bba4a2dde25f88d
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-27 03:21:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
       add_item = req.params["item"]
       if @@items.include?(add_item)
         @@cart << add_item
-        resp.write "Added #{add_item}\n"
+        resp.write "added #{add_item}\n"
       else
         resp.write "We don't have that item\n"
       end
